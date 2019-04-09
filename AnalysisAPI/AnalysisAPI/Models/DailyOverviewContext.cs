e33be97d4a6157b9656d25235bf859d2c4f7026d
Kaijubear <k.thomasbooth@gmail.com>
2019-04-09 20:20:36 +1200

    Added Context to daily overviews

---
+++ AnalysisAPI/AnalysisAPI/Models/DailyOverviewContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnalysisAPI.Models
+{
+    public class DailyOverviewContext : DbContext
+    {
+        public DailyOverviewContext(DbContextOptions<DailyOverviewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DailyOverview> Overviews { get; set; }
+    }
+}
