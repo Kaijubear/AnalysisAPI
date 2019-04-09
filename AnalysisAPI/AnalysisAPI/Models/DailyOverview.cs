9c6d1b01f3374c3c9abf1aee2b405c037a0c8d13
Kaijubear <k.thomasbooth@gmail.com>
2019-04-09 20:15:37 +1200

    Added DailyOverview model

---
+++ AnalysisAPI/AnalysisAPI/Models/DailyOverview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnalysisAPI.Models
+{
+    public class DailyOverview
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Sales { get; set; }
+        public double Budget { get; set; }
+
+    }
+}
